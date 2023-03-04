533f30a2084a9efd0a41dc16604190d327fb7798
Eric Sans <esansvi@gmail.com>
2023-03-04 01:08:33 +0100

    Principio de la refactorización

--- src/Criba.java
+++ src/Criba.java
@@ -1,46 +1,64 @@
 import java.util.Scanner;
-// Comentario para probar la ramificación
+
 public class Criba
 {
-    // Generar números primos de 1 a max
-    public static int[] generarPrimos (int max)
+    public static int[] generarPrimosHastaMax(int numeroMAX)
     {
-        int i,j;
-        if (max >= 2) {
+        if (numeroMAX >= 2) {
             // Declaraciones
-            int dim = max + 1; // Tamaño del array
-            boolean[] esPrimo = new boolean[dim];
-            // Inicializar el array
-            for (i=0; i<dim; i++)
-                esPrimo[i] = true;
-            // Eliminar el 0 y el 1, que no son primos
-            esPrimo[0] = esPrimo[1] = false;
-            // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (j=2*i; j<dim; j+=i)
-                        esPrimo[j] = false;
-                }
-            }
-            // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i=0; i<dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
-            // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (i=0, j=0; i<dim; i++) {
-                if (esPrimo[i])
-                    primos[j++] = i;
-            }
-            return primos;
-        } else { // max < 2
-            return new int[0];
+            int TamañoArray = numeroMAX + 1;
+            boolean[] esPrimo = new boolean[TamañoArray];
+            inicializarVectorPrimos(TamañoArray, esPrimo);
+            buscarPrimos(TamañoArray, esPrimo);
+            int cantidadPrimos = contarPrimos(TamañoArray, esPrimo);
+            return crearVectorPrimos(TamañoArray, esPrimo, cantidadPrimos);
+        } else {
             // Vector vacío
+            return new int[0];
         }
     }
+
+    private static int[] crearVectorPrimos(int TamañoArray, boolean[] esPrimo, int cantidadPrimos) {
+        int j;
+        int i;
+        int[] primos = new int[cantidadPrimos];
+        for (i=0, j=0; i< TamañoArray; i++) {
+            if (esPrimo[i])
+                primos[j++] = i;
+        }
+        return primos;
+    }
+
+    private static int contarPrimos(int TamañoArray, boolean[] esPrimo) {
+        int i;
+        int cuenta = 0;
+        for (i=0; i< TamañoArray; i++) {
+            if (esPrimo[i])
+                cuenta++;
+        }
+        return cuenta;
+    }
+
+    private static void inicializarVectorPrimos(int TamañoArray, boolean[] esPrimo) {
+        int i;
+        for (i=0; i< TamañoArray; i++)
+            esPrimo[i] = true;
+        // Eliminar el 0 y el 1, que no son primos
+        esPrimo[0] = esPrimo[1] = false;
+    }
+
+    private static void buscarPrimos(int TamañoArray, boolean[] esPrimo) {
+        int i;
+        int j;
+        for (i=2; i<Math.sqrt(TamañoArray)+1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (j=2*i; j< TamañoArray; j+=i)
+                    esPrimo[j] = false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
@@ -51,7 +69,7 @@ public class Criba
             if (i%10==0) System.out.println();
             System.out.print(i+1+"\t");
         }
-        vector=generarPrimos(dato);
+        vector= generarPrimosHastaMax(dato);
         System.out.println("\nVector de primos hasta:"+dato);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
